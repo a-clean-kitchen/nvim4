385c91e6d9a04ca0fb015e0ac1e03951f759ccad
a-clean-kitchen <mashaw.quade@gmail.com>
2024-02-23 16:27:27 -0500

    fix

--- modules/glow.nix
+++ modules/glow.nix
@@ -4,10 +4,12 @@ let
   cfg = config.vim.glow;
   inherit (lib) mkOption mkIf;
 in {
-  options.vim.glow = mkOption {
-    default = true;
-    type = lib.types.bool;
-    description = "Enable glow markup previewer";
+  options.vim.glow = {
+    enable = mkOption {
+      default = true;
+      type = lib.types.bool;
+      description = "Enable glow markup previewer";
+    };
   };
 
   config = mkIf cfg.enable {
