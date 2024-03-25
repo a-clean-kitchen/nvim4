911d827992f8969bc659fc41ec36421227d1d3d8
a-clean-kitchen <mashaw.quade@gmail.com>
2024-03-25 17:23:06 -0400

    we gottem

--- modules/completion.nix
+++ modules/completion.nix
@@ -3,6 +3,13 @@
 let
   cfg = config.vim.autocomplete;
   snippetOn = config.vim.luasnip.enable;
+  
+  snippetsRepo = pkgs.fetchFromGitHub {
+        owner = "a-clean-kitchen";
+        repo = "snipmate-snippets";
+        rev = "main";
+        hash = "sha256-QAY4N3+MJK2CE1ikMvGQQ/k8t7GF8ID+mbaULvPh7x0=";
+  };
 
   inherit (lib) mkIf mkOption types;
   inherit (lib.my) withPlugins writeIf indent;
@@ -38,97 +45,73 @@ in
     '';
  
     vim.luaConfigRC = /*lua*/ ''
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+      require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "${snippetsRepo}/snippets" } })
       cmp.setup({
-      ${let
-          enabled = config.vim.luasnip.enable;
-        in 
-        indent "  " (writeIf enabled /*lua*/ ''
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-        '')
-      }
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
         formatting = {
           format = lspkind.cmp_format({with_text = true, maxwidth = 50}) 
         },
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), 
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+            -- that way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
       })
     '';
-
-#     vim.luaConfigRC = /*lua*/ ''
-#       local has_words_before = function()
-#           if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-#           local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-#         return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-#       end      
-# 
-#       local feedkey = function(key, mode)
-#         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-#       end
-# 
-#       cmp.setup({
-#         snippet = {
-#           expand = function(args)
-#             vim.fn["vsnip#anonymous"](args.body)
-#           end,
-#         },
-#         sources = cmp.config.sources({
-#           { name = 'nvim_lsp' },
-#           { name = 'path' },
-#           { name = 'treesitter' },
-#           { name = 'vsnip' },
-#         }, {
-#           { name = 'buffer' },
-#         }),
-#         mapping = {
-#           ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-#           ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-#           ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-#           ['<C-y>'] = cmp.config.disable,
-#           ['<C-e>'] = cmp.mapping({
-#             i = cmp.mapping.abort(),
-#             c = cmp.mapping.close(),
-#           }),
-#           ['<CR>'] = cmp.mapping.confirm({
-#             select = true,
-#           }),
-#           ['<Tab>'] = cmp.mapping(function(fallback)
-#             if cmp.visible() then
-#               cmp.select_next_item()
-#             elseif vim.fn['vsnip#available'](1) == 1 then
-#               feedkey("<Plug>(vsnip-expand-or-jump)", "")
-#             elseif has_words_before() then
-#               cmp.complete()
-#             else
-#               fallback()
-#             end
-#           end, { 'i', 's' }),
-#       
-#           ['<S-Tab>'] = cmp.mapping(function(fallback)
-#             if cmp.visible() then
-#               cmp.select_prev_item()
-#             elseif vim.fn['vsnip#available'](-1) == 1 then
-#               feedkeys("<Plug>(vsnip-jump-prev)", "")
-#             end
-#           end, { 'i', 's' })
-#         },
-#         formatting = {
-#           format = lspkind.cmp_format({with_text = true, maxwidth = 50}) 
-#         },
-#         completion = {
-#           completeopt = 'menu,menuone,noinsert',
-#         },
-#       }) 
-# 
-#       cmp.setup.cmdline(':', {
-#         mapping = cmp.mapping.preset.cmdline(),
-#         sources = cmp.config.sources({
-#           { name = 'path' }
-#         }, {
-#           { name = 'cmdline' }
-#         })
-#       })
-#     '';
   };
 }
--- modules/luasnip.nix
+++ modules/luasnip.nix
@@ -6,15 +6,14 @@ let
   inherit (lib) mkIf mkOption types;
 in
 {
-  options.vim = {
-    luasnip = {
+  options.vim.luasnip = {
       enable = mkOption {
         type = types.bool;
         default = true;
         description = "enable luasnip";
       };
     };
-  };
+  
 
   config = mkIf cfg.enable {
     vim.startPlugins = with pkgs.myVimPlugins; [
@@ -24,6 +23,18 @@ in
     vim.startLuaConfigRC = /*lua*/ ''
       local status, luasnip = pcall(require, "luasnip")
       if (not status) then return end
+    '';
+
+    vim.luaConfigRC = /*lua*/ ''
+      vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
+      vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
+      vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
+
+      vim.keymap.set({"i", "s"}, "<C-E>", function()
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end, {silent = true})
     '';
   };
 }
