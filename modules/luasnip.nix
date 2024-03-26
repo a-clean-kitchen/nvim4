d7e0007cdb66ec092010d6d1486923f1b2f0e1d8
a-clean-kitchen <Mashaw.Quade@gmail.com>
2024-03-25 22:20:44 -0400

    small org

--- modules/luasnip.nix
+++ modules/luasnip.nix
@@ -26,6 +26,8 @@ in
     '';
 
     vim.luaConfigRC = /*lua*/ ''
+      require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "${ ./snippets }" } })
+
       vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
