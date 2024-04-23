dd5bfcb7e3267d449b4c88a1ca015b9e1c86ef2e
Quade Mashaw <50602948+a-clean-kitchen@users.noreply.github.com>
2024-04-23 13:22:46 -0400

    Snippets (#3)
    * blank module
    
    * =
    
    * letlib
    
    * whoopsie :)
    
    * this is a good spot

--- modules/autopairs.nix
+++ modules/autopairs.nix
@@ -8,14 +8,49 @@ in {
       autopairs
     ];
 
-    vim.startLuaConfigRC = ''
+    vim.startLuaConfigRC = /*lua*/ ''
       local status, autopairs = pcall(require, "nvim-autopairs")
+      if (not status) then return end
+
+      local status, APRule = pcall(require, "nvim-autopairs.rule")
+      if (not status) then return end
+
+      local status, APcond = pcall(require, "nvim-autopairs.conds")
       if (not status) then return end
     '';
     
-    vim.luaConfigRC = ''
+    vim.luaConfigRC = /*lua*/ ''
       autopairs.setup({
         disable_filetype = { "TelescopePrompt" , "vim" },
+      })
+      autopairs.add_rules({
+        APRule("=", "")
+          :with_pair(APcond.not_inside_quote())
+          :with_pair(function(opts)
+              local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+              if last_char:match("[%w%=%s]") then
+                  return true
+              end
+              return false
+          end)
+          :replace_endpair(function(opts)
+              local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+              local next_char = opts.line:sub(opts.col, opts.col)
+              next_char = next_char == " " and "" or " "
+              if prev_2char:match("%w$") then
+                  return "<bs> =" .. next_char
+              end
+              if prev_2char:match("%=$") then
+                  return next_char
+              end
+              if prev_2char:match("=") then
+                  return "<bs><bs>=" .. next_char
+              end
+              return ""
+          end)
+          :set_end_pair_length(0)
+          :with_move(APcond.none())
+          :with_del(APcond.none())
       })
     '';
   };
