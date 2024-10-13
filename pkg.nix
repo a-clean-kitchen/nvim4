3344376ff2ccf5c770b557a89d35f363bafbeded
a-clean-kitchen <mashaw.quade@gmail.com>
2024-10-13 07:33:47 -0400

    fixd

--- pkg.nix
+++ pkg.nix
@@ -63,7 +63,7 @@ let
       neovim = pkgs.wrapNeovim vim.package {
         inherit (vim) viAlias vimAlias;
         configure = {
-          customRC = builtins.trace finalConfigRC finalConfigRC;
+          customRC = finalConfigRC;
 
           packages.myVimPackage = {
             start = completedPlugins;
