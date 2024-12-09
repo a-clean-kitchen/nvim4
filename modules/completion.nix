a1daf52b6be82b2bd38b3390ec9c7c74611a8ebb
a-clean-kitchen <Mashaw.Quade@gmail.com>
2024-12-09 17:57:53 -0500

    we out this bih

--- modules/completion.nix
+++ modules/completion.nix
@@ -43,7 +43,6 @@ in
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
-      require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "${ ./snippets }" } })
       cmp.setup({
         snippet = {
           expand = function(args)
--- modules/luasnip.nix
+++ modules/luasnip.nix
@@ -7,32 +7,31 @@ let
 in
 {
   options.vim.luasnip = {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
-        description = "enable luasnip";
-      };
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "enable luasnip";
     };
+    snipConfig = mkOption {
+      type = types.lines;
+      default = "";
+
+    };
+  };
   
 
   config = mkIf cfg.enable {
     vim.startPlugins = with pkgs.myVimPlugins; [
       luasnip
-      vim-react-snippets
     ];
 
     vim.startLuaConfigRC = /*lua*/ ''
       local status, luasnip = pcall(require, "luasnip")
       if (not status) then return end
-
-      local status, reactSnippets = pcall(require, "vim-react-snippets")
-      if (not status) then return end
     '';
 
     vim.luaConfigRC = /*lua*/ ''
-      reactSnippets.lazy_load()
-
-      require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "${ ./snippets }" } })
+      require("luasnip.loaders.from_vscode").load({ paths = { "${ ./snippets }" } })
 
       vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
       vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
